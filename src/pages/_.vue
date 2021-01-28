25063054780f38974925a27c09b95fdda17cb600
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-28 20:05:45 +0700

    feat: create not-found page

---
+++ src/pages/_.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="NotFound">
+    <p class="FontSize-2xlarge MarginBottom">Page Not Found</p>
+
+    <nuxt-link
+      id="BtnBackToLanding"
+      class="Button Button--primary"
+      to="/landing"
+    >
+      Take me away
+    </nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "NotFound",
+});
+</script>
+
+<style lang="scss" scoped>
+.NotFound {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
