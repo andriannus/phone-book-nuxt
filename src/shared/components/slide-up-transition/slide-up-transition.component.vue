1177f49e1dddd7c89aaae2c40db6ebb8c389c9f7
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-28 21:47:51 +0700

    feat: create slide-up-transition component

---
+++ src/shared/components/slide-up-transition/slide-up-transition.component.vue
@@ -0,0 +1,8 @@
+<template>
+  <transition name="SlideUp">
+    <slot></slot>
+  </transition>
+</template>
+
+<script lang="ts" src="./slide-up-transition.component.ts"></script>
+<style lang="scss" scoped src="./slide-up-transition.component.scss"></style>
