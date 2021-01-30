541747a8eb68f68086e2c3687ad98f32e0a3e781
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-30 10:34:04 +0700

    feat: create button-to-top component

---
+++ src/shared/components/button-to-top/button-to-top.component.ts
@@ -0,0 +1,57 @@
+import {
+  defineComponent,
+  onMounted,
+  onUnmounted,
+  reactive,
+} from "@nuxtjs/composition-api";
+
+import { ButtonToTopState } from "./button-to-top.model";
+
+import QSlideUpTransition from "@/app/shared/components/slide-up-transition/slide-up-transition.component.vue";
+import {
+  SCROLL_INTERVAL,
+  SCROLL_STEP_DIVIDER,
+} from "@/shared/constants/scroll.constant";
+
+export default defineComponent({
+  name: "ButtonToTop",
+
+  components: {
+    QSlideUpTransition,
+  },
+
+  setup() {
+    const state = reactive<ButtonToTopState>({
+      isShowButton: false,
+    });
+
+    onMounted((): void => {
+      window.addEventListener("scroll", toggleToTopButton);
+    });
+
+    onUnmounted((): void => {
+      window.removeEventListener("scroll", toggleToTopButton);
+    });
+
+    function toggleToTopButton(): void {
+      const { body, documentElement } = document;
+      const isValidBody = body.scrollTop > 300;
+      const isValidDocumentElement = documentElement.scrollTop > 300;
+
+      state.isShowButton = isValidBody || isValidDocumentElement;
+    }
+
+    function onClickButton(): void {
+      const scrollStep = -window.scrollY / SCROLL_STEP_DIVIDER;
+      const scrollInterval = setInterval((): void => {
+        if (window.scrollY === 0) {
+          return clearInterval(scrollInterval);
+        }
+
+        window.scrollBy(0, scrollStep);
+      }, SCROLL_INTERVAL);
+    }
+
+    return { onClickButton, state };
+  },
+});
---
+++ src/shared/components/button-to-top/button-to-top.component.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-slide-up-transition>
+    <button
+      v-if="state.isShowButton"
+      class="ButtonToTop Button Button--dark Button--rounded Button--elevated"
+      @click="onClickButton"
+    >
+      Take me to fly up
+    </button>
+  </q-slide-up-transition>
+</template>
+
+<script lang="ts" src="./button-to-top.component.ts"></script>
+<style lang="scss" scoped src="./button-to-top.component.scss"></style>
