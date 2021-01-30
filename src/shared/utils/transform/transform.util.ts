ad9d36e1230b86595dd5cb40d970a7c211d2cf1c
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-30 11:16:02 +0700

    feat: create transform util

---
+++ src/shared/utils/transform/transform.util.ts
@@ -0,0 +1,11 @@
+import { Location, Name } from "@/shared/model/random-user.model";
+
+export function transformAddress(location: Location): string {
+  const { city, postcode, state } = location;
+  return `${city}, ${state}, ${postcode}`;
+}
+
+export function transformFullName(name: Name): string {
+  const { first, last, title } = name;
+  return `${title} ${first} ${last}`;
+}
