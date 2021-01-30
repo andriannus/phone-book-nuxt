030e92f7f2339267627f5efa79a60c1f9246d288
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-01-30 10:38:17 +0700

    feat: create button-to-left component

---
+++ src/shared/components/button-to-left/button-to-left.component.ts
@@ -0,0 +1,65 @@
+import {
+  defineComponent,
+  onMounted,
+  onUnmounted,
+  reactive,
+} from "@nuxtjs/composition-api";
+
+import { ButtonToLeftState } from "./button-to-left.model";
+
+import QSlideUpTransition from "@/app/shared/components/slide-up-transition/slide-up-transition.component.vue";
+import {
+  SCROLL_INTERVAL,
+  SCROLL_STEP_DIVIDER,
+} from "@/shared/constants/scroll.constant";
+
+export default defineComponent({
+  name: "ButtonToLeft",
+
+  components: {
+    QSlideUpTransition,
+  },
+
+  props: {
+    selector: {
+      type: String,
+      default: "body",
+    },
+  },
+
+  setup(props) {
+    const state = reactive<ButtonToLeftState>({
+      container: null,
+      isShowButton: false,
+    });
+
+    onMounted((): void => {
+      state.container = document.querySelector(props.selector);
+      state.container?.addEventListener("scroll", toggleToLeftButton);
+    });
+
+    onUnmounted((): void => {
+      state.container?.removeEventListener("scroll", toggleToLeftButton);
+    });
+
+    function toggleToLeftButton(): void {
+      if (!state.container) return;
+      state.isShowButton = state.container.scrollLeft >= 900;
+    }
+
+    function onClickButton(): void {
+      if (!state.container) return;
+
+      const scrollStep = state.container.scrollLeft / SCROLL_STEP_DIVIDER;
+      const scrollInterval = setInterval((): void => {
+        if (state.container!.scrollLeft === 0) {
+          window.clearInterval(scrollInterval);
+        }
+
+        state.container!.scrollLeft -= scrollStep;
+      }, SCROLL_INTERVAL);
+    }
+
+    return { onClickButton, state };
+  },
+});
---
+++ src/shared/components/button-to-left/button-to-left.component.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-slide-up-transition>
+    <button
+      v-if="state.isShowButton"
+      class="ButtonToLeft Button Button--dark Button--rounded Button--elevated"
+      @click="onClickButton"
+    >
+      Give me back to the left
+    </button>
+  </q-slide-up-transition>
+</template>
+
+<script lang="ts" src="./button-to-left.component.ts"></script>
+<style lang="scss" scoped src="./button-to-left.component.scss"></style>
