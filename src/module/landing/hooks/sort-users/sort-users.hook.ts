38f799d864fbe764337888abd07bdd2660bfccff
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-02-05 08:34:07 +0700

    feat: create sort-users hook

---
+++ src/module/landing/hooks/sort-users/sort-users.hook.ts
@@ -0,0 +1,63 @@
+import { reactive } from "@nuxtjs/composition-api";
+
+import { SortUsersState } from "./sort-users.model";
+
+import { ColorfulUser } from "../colorful-users";
+
+import { UserColor, UserSort } from "@/shared/enums/user.enum";
+
+export function useSortUsers(
+  users: ColorfulUser[],
+  sort: UserSort,
+): ColorfulUser[] {
+  const state = reactive<SortUsersState>({
+    users,
+    sort,
+  });
+
+  (function handleUserSort(): void {
+    switch (state.sort) {
+      case UserSort.City: {
+        sortByCity();
+        break;
+      }
+      case UserSort.Color: {
+        sortByColor();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  })();
+
+  function sortByCity(): void {
+    state.users.sort((a: ColorfulUser, b: ColorfulUser): 1 | -1 | 0 => {
+      const cityA = a.location.city.toUpperCase();
+      const cityB = b.location.city.toUpperCase();
+
+      if (cityA < cityB) return -1;
+      if (cityA > cityB) return 1;
+
+      return 0;
+    });
+  }
+
+  function sortByColor(): void {
+    const greenUsers = state.users.filter((user: ColorfulUser): boolean => {
+      return user.color === UserColor.Green;
+    });
+
+    const blueUsers = state.users.filter((user: ColorfulUser): boolean => {
+      return user.color === UserColor.Blue;
+    });
+
+    const redUsers = state.users.filter((user: ColorfulUser): boolean => {
+      return user.color === UserColor.Red;
+    });
+
+    state.users = [...greenUsers, ...blueUsers, ...redUsers];
+  }
+
+  return state.users;
+}
