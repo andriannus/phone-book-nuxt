5f64f782842ae44d2c24ceede4b792af3dbe403b
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-02-05 09:31:17 +0700

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-    "^~/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^~/(.*)$": "<rootDir>/src/$1",
     "^vue$": "vue/dist/vue.common.js",
   },
   moduleFileExtensions: ["ts", "js", "vue", "json"],
@@ -12,7 +12,11 @@ module.exports = {
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    "<rootDir>/components/**/*.vue",
-    "<rootDir>/pages/**/*.vue",
+    "<rootDir>/src/**/*.{ts,js,vue}",
+    "!**/index.ts",
+    "!**/*.{d,enum,model,constant}.ts",
+    "!<rootDir>/src/static/**",
+    "!<rootDir>/src/test/**",
+    "!<rootDir>/src/shared/assets/**",
   ],
 };
