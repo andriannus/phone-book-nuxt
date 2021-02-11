4be203de3403bad1371dc05d0ee179e782dedbc5
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-02-11 21:46:32 +0700

    chore: add meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,12 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Phone Book with Nuxt.js, TypeScript, and Composition API powered by RandomUser.me",
+      },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
